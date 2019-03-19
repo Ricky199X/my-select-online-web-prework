7ff3d35e09b74959168749cce6bbe00407e154cc
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-19 02:27:55 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,11 @@
 def my_select(collection)
   result = []
   i = 0
-  while i < collection.length do
-    result << yield(array[i])
+  while i < collections.length
+    if yield(array[i]) == true
+    result << array[i]
+      else
+      end
     i += 1
   end
   result
